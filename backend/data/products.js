4e6973c5a7ce51f2fe92b765ca45271739dd7b56
Deepak <deepsk3697@gmail.com>
2021-10-26 11:44:48 +0530

    Prepare for deployment

---
+++ backend/data/products.js
@@ -0,0 +1,78 @@
+const products = [
+    {
+       
+        name: 'Alexa',
+        image: '/images/alexa.jpg',
+        description: 'Alexa is Amazon’s cloud-based voice service available on hundreds of millions of devices from Amazon and third-party device manufacturers. With Alexa, you can build natural voice experiences that offer customers a more intuitive way to interact with the technology they use every day',
+        brand: 'Amazon',
+        category: 'Electronics',
+        price: 2200,
+        countInStock:4,
+        rating: 4.5,
+        numReviews: 3,
+    },
+    {
+        
+        name:'Asus Gaming Laptop',
+        image:'/images/asus_gaming.jpg',
+        description:'GeForce RTX™ 3080 Laptop GPU High performance gaming laptop',
+        brand:'ROG',
+        category:'Electronics',
+        price: 159999,
+        countInStock: 2 ,
+        rating: 4,
+        numReviews : 5
+
+    },
+    {
+        
+        name:'Iphone 13',
+        image:'/images/iphone13.jpg',
+        description:'the iPhone 13 and iPhone 13 mini are Apple newest flagship iPhones on the more affordable end, and are being sold alongside the more expensive iPhone 13 Pro and iPhone 13 Pro Max. The iPhone 13 and iPhone 13 mini are ideal for those who dont need pro-level camera features.',
+        brand:'Apple',
+        category:'Electronics',
+        price: 69999,
+        countInStock:4 ,
+        rating: 4.2,
+        numReviews : 5
+
+    },
+    {
+        
+        name:'Denim Jacket',
+        image:'/images/jacket.jpg',
+        description:'Denim Jacket',
+        brand:'Denim',
+        category:'Clothing',
+        price: 1500,
+        countInStock:5 ,
+        rating: 3.7,
+        numReviews : 5
+    },
+    {
+        
+        name:'Puma one9',
+        image:'/images/puma_one8.jpg',
+        description:'Introducing new Puma one8 brand , casual running shoes.',
+        brand:'Puma',
+        category:'Shoes',
+        price: 2500,
+        countInStock:3 ,
+        rating: 4,
+        numReviews : 5
+    },
+    {
+        
+        name:'Samsung Galaxy Z Fold 3',
+        image:'/images/Samsung-Galaxy-Z-Fold-3.jpg',
+        description:'New folding tech smartphone with high quality inbuilt mechanisms.',
+        brand:'Samsung',
+        category:'Mobiles',
+        price: 74999,
+        countInStock:6 ,
+        rating: 4.6,
+        numReviews : 6
+    }
+]
+
+export default products
